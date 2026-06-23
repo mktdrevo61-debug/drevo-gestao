31c68e69db328f38d2bf3d2ef6221ed3c5a10ac2
Drevo Gestao <contato@drevo.com.br>
2026-06-23 17:35:26 -0300

    Implementando Firebase Web Push Notifications (FCM)

--- sw.js
+++ sw.js
@@ -1,3 +1,29 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyD_TaKudierPObWMsgmTJ9Cx4XdXLZ4meg",
+    authDomain: "drevo-compras.firebaseapp.com",
+    projectId: "drevo-compras",
+    storageBucket: "drevo-compras.firebasestorage.app",
+    messagingSenderId: "954205790898",
+    appId: "1:954205790898:web:fa632733b0468ea51d5fab"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[sw.js] Received background message ', payload);
+    const notificationTitle = payload.notification.title || "Notificação";
+    const notificationOptions = {
+        body: payload.notification.body || "",
+        icon: 'icon-192.png',
+        badge: 'icon-192.png',
+        vibrate: [200, 100, 200, 100, 200, 100, 200]
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
 self.addEventListener('install', event => {
     self.skipWaiting();
 });
